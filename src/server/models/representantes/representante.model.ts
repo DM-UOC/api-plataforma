cc70d3c46500acc1d12492b271c1377d9d9c465f
Diego Martinez <dmartinez@uoc.edu>
2020-11-22 08:50:40 -0500

    Cambios: Getion de distintos perfiles para ingreso de respectivos datos y afectacion de colecciones en BD

--- src/server/models/representantes/representante.model.ts
+++ src/server/models/representantes/representante.model.ts
@@ -1,9 +1,14 @@
 import { prop } from "@typegoose/typegoose";
 import { Types } from "mongoose";
+import { HijoModel } from "./hijo.model";
 
 export class RepresentanteModel {
 
     @prop({ required: true })
     public usuario_id?: Types.ObjectId;
-    
+
+    @prop({ required: true, type: HijoModel })
+    public hijos?: HijoModel[];
+
+
 }
