93653206bd9968db1241445ffd060cc19178f969
Diego Martinez <dmartinez@uoc.edu>
2020-11-22 08:50:22 -0500

    Cambios: Getion de distintos perfiles para ingreso de respectivos datos y afectacion de colecciones en BD

--- src/server/models/materias/materia.model.ts
+++ src/server/models/materias/materia.model.ts
@@ -1,1 +1,15 @@
-export class MateriaModel {}
+import { prop } from "@typegoose/typegoose";
+import { Types } from "mongoose";
+import { AuditoriaModel } from "../comuns/auditoria.model";
+
+export class MateriaModel {
+
+    public readonly _id: Types.ObjectId;
+
+    @prop({ required: true, trim: true })
+    public descripcion?: string;
+
+    @prop({ type: AuditoriaModel })
+    public auditoria: AuditoriaModel;
+    
+}
